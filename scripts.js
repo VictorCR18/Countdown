c6bef7a7b37c540caf2167c83a40d7d10bb3a15d
VictorCR18 <Cavalcantevictor2003@gmail.com>
2022-08-05 00:16:48 -0300

    versao 4, agora com modal com formulario

--- scripts.js
+++ scripts.js
@@ -15,8 +15,41 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
+
 window.onload = function () {
     let duration = 737068; // Converter para segundos
     display = document.querySelector('#timer'); // selecionando o timer
     startTimer(duration, display); // iniciando o timer
 };
+
+const modalFuction = (event) => {
+
+    let modal = document.getElementById("modal"); // Get the modal
+
+    let button = document.getElementById("button"); // Get the button that opens the modal
+
+    let submit = document.getElementById("bnt");
+
+    let span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+    // When the user clicks the button, open the modal 
+    button.onclick = function () {
+        modal.style.display = "flex";
+    }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    bnt.onclick = function () {
+        alert("Enviado com sucesso!");
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+window.onclick = function (event) {
+    modalFuction(event);
+}
